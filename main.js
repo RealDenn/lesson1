4628206eb6a3c7ea08da4cd7e3608f169e7c41af
Денис <denkul72@gmail.com>
2019-02-11 23:00:06 +0300

    Добавил перерисовку
    Добавил перерисовку секундной стрелки и добавил мат.расчёты для минутной стрелки

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ var circle = new Path2D;
 circle.arc(150, 150, 150, 0, 2 * Math.PI);
 context.stroke(circle);
 
-var R = 300/2, d,angle,pX,pY,qX,qY;
+
+    
+   
+
+function drawWatch() {
+    context.clearRect(0,0,300,300);
+    circle = new Path2D();
+    circle.arc(150, 150, 150, 0, 2 * Math.PI);
+    context.stroke(circle);
+
+    var R = 300/2, d,angle,pX,pY,qX,qY;
 
 for(d = 0; d < 60; ++d) {
     angle = (d/60) * (2*Math.PI);
@@ -22,8 +32,8 @@ for(d = 0; d < 60; ++d) {
     context.moveTo(pX,pY);
     context.lineTo(qX,qY);
     context.stroke();
-   
-}
+
+} 
 var date = new Date(), hours,minutes,seconds;
 seconds = date.getSeconds();
 minutes = date.getMinutes();
@@ -33,10 +43,27 @@ var sX,sY;
 lineS = new Path2D();
 var  secondsAngle = (seconds / 60) * (2 * Math.PI)
 secondsAngle = Math.PI / 2 - secondsAngle;
+
+var hoursAngle = ((hours % 12) / 12) * (2 * Math.PI)
+hoursAngle = Math.PI / 2 - hoursAngle;
+
 sX = Math.cos(secondsAngle) * R * 0.85;
 sY = -Math.sin(secondsAngle) * R * 0.85;
 sX += R; sY += R;
 
+var mX,mY;
+lineM = new Path2D();
+var minutesAngle = (minutes / 60) * (2 * Math.PI)
+minutesAngle = Math.PI / 2 - minutesAngle;
+
+mX = Math.cos(minutesAngle) * R * 0.75;
+mY = -Math.sin(minutesAngle) * R * 0.75;
+mX += R; mY += R;
+
 lineS.moveTo(150,150);
 lineS.lineTo(sX,sY);
 context.stroke(lineS);
+
+setTimeout(drawWatch,1000);
+}
+drawWatch();
